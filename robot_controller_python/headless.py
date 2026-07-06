85365655480fb8f30eb7d1d629838a6706fb6124
Maxime Capdouze <94197358+maxcap12@users.noreply.github.com>
2026-07-06 16:40:55 +0200

    add position correction for robot placement

--- robot_controller_python/headless.py
+++ robot_controller_python/headless.py
@@ -1,11 +1,17 @@
 from isaacsim.core.api.world import World
 from pxr import UsdLux, UsdGeom, Sdf, Gf
+from omni.physx import get_physx_scene_query_interface
 import omni.usd
+import omni.kit.app
+import carb
 from .robot_controller import RobotController
 from .map_creator import load_map
 
 
 class HeadlessRunner:
+    SPAWN_CLEARANCE = {"spot": 0.6, "go1": 0.4, "go2": 0.4}
+    DEFAULT_CLEARANCE = 0.4
+    
     def __init__(self, physics_dt=1/60., rendering_dt=1/60.):
         self.world = World(
             stage_units_in_meters=1.0,
@@ -34,6 +40,47 @@ class HeadlessRunner:
         light.CreateIntensityAttr(3000.0)
         light.CreateColorAttr(Gf.Vec3f(1.0, 1.0, 1.0))
         UsdGeom.Xformable(light.GetPrim()).AddRotateXYZOp().Set(Gf.Vec3f(-45.0, 0.0, 0.0))
+
+    def correct_robot_position(self, clearence=None, start_offset=10.0, max_distance=50.0):
+        if self.robot is None: return
+
+        robot_name = self.controller.robot_name
+        position, orientation = self.robot.get_world_pose()
+        x, y, z = float(position[0]), float(position[1]), float(position[2])
+
+        surface_z = self._surface_below(
+            x, y, z, ignore_prefix=f"/{robot_name}",
+            start_offset=start_offset, max_distance=max_distance
+        )
+
+        if surface_z is None: return
+
+        if clearence is None:
+            clearence = self.SPAWN_CLEARANCE.get(robot_name, self.DEFAULT_CLEARANCE)
+
+        new_pos = [x, y, surface_z + clearance]
+        self.robot.set_world_pose(position=new_pos, orientation=orientation)
+        self.robot.set_default_state(position=new_pos, orientation=orientation)
+
+    def _surface_below(self, x, y, z, ignore_prefix=None, start_offset=10.0, max_distance=50.0):
+        origin = carb.Float3(float(x), float(y), float(z + start_offset))
+        direction = carb.Float3(0.0, 0.0, -1.0)
+
+        best = {"z": None, "dist": None}
+
+        def report(hit):
+            if ignore_prefix and hit.collision.startswith(ignore_prefix):
+                return True
+            if best["dist"] is None or hit.distance < best["dist"]:
+                best["dist"] = hit.distance
+                best["z"] = float(hit.position[2])
+
+            return True
+
+        get_physx_scene_query_interface().raycast_all(
+            origin, direction, float(max_distance), report
+        )
+        return best["z"]
 
     def run(self, duration_s, render=True):
         n = int(duration_s / self.world.get_physics_dt())
